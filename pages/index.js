4ace57da49c59e4cef892a26e3530b12b0f2224e
Mike Nikles <mikenikles@gmail.com>
2017-06-25 20:18:59 -0700

    Add dependency: ESLint.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import MainLayout from '../components/layouts/main'
 // The LandingPage is a placeholder until we go live
 import LandingPage from '../components/landing-page'
-
 import withData from '../utils/apollo/with-data'
 
 export default withData((props) => (
