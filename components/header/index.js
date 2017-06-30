4a0dd012fdd89d43f36bffd809c9aa6d8f1083f3
Mike Nikles <mikenikles@gmail.com>
2017-06-29 21:58:53 -0700

    Add a Header component placeholder.

---
+++ components/header/index.js
@@ -0,0 +1,10 @@
+import Box from '../box'
+
+const Header = () => (
+  <Box bg="#d2654b" color="white"
+      p={1}>
+    Dittly
+  </Box>
+)
+
+export default Header
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
 import MainLayout from '../components/layouts/main'
-// The LandingPage is a placeholder until we go live
-import LandingPage from '../components/landing-page'
+import Header from '../components/header'
 import withData from '../utils/apollo/with-data'
 
 export default withData((props) => (
   <MainLayout>
-    <LandingPage />
+    <Header />
   </MainLayout>
 ))
