13f2688ccc0fd1c385d3b4e952b4111ae8988ef5
Mike Nikles <mikenikles@gmail.com>
2017-07-09 21:07:38 -0700

    Remove webpack since it's not directly used

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /* eslint-env node */
 /* eslint-disable import/no-commonjs */
-const webpack = require('webpack')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
