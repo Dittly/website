e58ffca987c7428712d829d3e9a9928a6a0241c0
Magitron <maggiecaspar@gmail.com>
2017-08-28 16:56:14 +0700

    add storybook and a couple of components

---
+++ stories/Search.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const searchStyles = {
+  backgroundColor: 'white',
+  borderBottom: '1px solid #EDEDED',
+  padding: '4px',
+  paddingLeft: '28px',
+  fontFamily: 'Rubik',
+  color: '#9B9B9B',
+  fontSize: '14px',
+};
+
+const Search = () => (
+  <div
+   style={searchStyles}
+  >
+      Search
+  </div>
+);
+
+export default Search
---
+++ stories/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import Button from './Button';
+import Search from './Search';
+import Create from './Create';
+
+import { Welcome } from '@storybook/react/demo';
+
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+
+storiesOf('Button', module)
+  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+
+storiesOf('Search', module)
+  .add('with text', () => <Search>Search</Search>);
+
+storiesOf('Create', module)
+  .add('with text', () => <Create></Create>);
