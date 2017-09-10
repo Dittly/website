b90eba99e7ea841c3e09a8d42d775fea9c126e04
Mike Nikles <mikenikles@gmail.com>
2017-09-09 20:51:58 -0700

    Add base styles to Storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,6 +2,9 @@ import { configure } from '@storybook/react';
 
 const req = require.context('../components', true, /\-stories\.js$/)
 
+// Adds normalize and general styles
+import '../styles/base-styles'
+
 function loadStories() {
   req.keys().forEach((filename) => req(filename))
 }
