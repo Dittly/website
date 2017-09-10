e9875949514c896a222691a847d3fad4ff91c905
Mike Nikles <mikenikles@gmail.com>
2017-09-09 19:37:57 -0700

    Use defaultProps for the main layout.

--- components/layouts/index.js
+++ components/layouts/index.js
@@ -5,15 +5,20 @@ export const layouts = {
   main
 }
 
-const getLayout = name => layouts[name || 'main']
+const getLayout = name => layouts[name]
 
 const WithLayout = ({children, name}) => {
   const Layout = getLayout(name)
   return <Layout name={name}>{children}</Layout>
 }
+
 WithLayout.propTypes = {
   children: PropTypes.node.isRequired,
   name: PropTypes.oneOf(Object.keys(layouts))
+}
+
+WithLayout.defaultProps = {
+  name: 'main'
 }
 
 export default WithLayout
