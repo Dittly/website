25975122f5a9caf9eb8851379a7ce2639232dd11
Magitron <maggiecaspar@gmail.com>
2017-09-17 14:36:11 +0700

    add max with to project

--- components/project/list-sc.js
+++ components/project/list-sc.js
@@ -1,7 +1,9 @@
 import styled from 'styled-components'
-import {media} from '../../styles/media'
+import {sizes, media} from '../../styles/media'
 
 const ProjectListSC = styled.div`
+  max-width: ${sizes.phone}px;
+  margin: 0 auto;
   ${media.tablet`
     display: grid;
     grid-template-columns: repeat(2, 1fr);
