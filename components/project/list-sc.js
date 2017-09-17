2c2acc0767120b3bdd71a4d975c1dbf2b0f7af32
Mike Nikles <mikenikles@gmail.com>
2017-09-17 11:36:06 -0700

    Remove duplicated CSS styles.

--- components/project/list-sc.js
+++ components/project/list-sc.js
@@ -9,32 +9,20 @@ const ProjectListSC = styled.div`
   grid-gap: 20px;
 
   ${media.phone`
-    display: grid;
     grid-template-columns: repeat(2, 1fr);
-    grid-gap: 20px;
     max-width: ${sizes.phone}px;
-    margin: 0 auto;
   `};
   ${media.tablet`
-    display: grid;
     grid-template-columns: repeat(3, 1fr);
-    grid-gap: 20px;
     max-width: ${sizes.tablet}px;
-    margin: 0 auto;
   `};
   ${media.desktop`
-    display: grid;
     grid-template-columns: repeat(3, 1fr);
-    grid-gap: 20px;
     max-width: ${sizes.desktop}px;
-    margin: 0 auto;
   `};
   ${media.giant`
-    display: grid;
     grid-template-columns: repeat(4, 1fr);
-    grid-gap: 20px;
     max-width: ${sizes.giant}px;
-    margin: 0 auto;
   `};
 `
 
