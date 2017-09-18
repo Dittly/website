bd5a88db1298650e3edf3e717d1a4c613f7efc8a
Magitron <maggiecaspar@gmail.com>
2017-09-18 16:58:57 +0700

    add media query to header-bar, style header to suit

--- components/header/index-sc.js
+++ components/header/index-sc.js
@@ -5,6 +5,9 @@ import {neutrals} from '../../styles/constants'
 const HeaderSC = styled.header`
   background-color: ${neutrals.neutral10};
   color: ${neutrals.neutral50};
+  position: relative;
+  border-bottom: 1px solid;
+  border-color: ${neutrals.neutral20};
 `
 
 export default HeaderSC
