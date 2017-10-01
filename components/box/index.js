3e3f61246c1f0c03fa669fcbec066c6beafb3af8
Magitron <maggiecaspar@gmail.com>
2017-10-01 14:41:55 +0700

    add tablet/desktop styling to login/signup pages

--- components/box/index.js
+++ components/box/index.js
@@ -2,5 +2,7 @@ import styled from 'styled-components'
 import {space, width, fontSize, color} from 'styled-system'
 
 export default styled.div`
+  margin: ${props => (props.center ? '0 auto' : '')};
+
   ${space} ${width} ${fontSize} ${color};
 `
