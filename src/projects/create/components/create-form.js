8cc1ac1306a9225229537fe1730f0c99cc179d3e
mahongru <mahongru.ca@gmail.com>
2017-10-03 21:34:57 -0700

    copied project create related components to feature specific components directory

---
+++ src/projects/create/components/create-form.js
@@ -0,0 +1,56 @@
+import PropTypes from 'prop-types'
+
+import Button from '/components/button'
+import Input from '/components/input'
+
+const CreateForm = ({projectTypes, onSubmit}) => (
+  <form onSubmit={onSubmit}>
+    <label htmlFor="create-project-title">
+      <br />Project Title*<br />
+      <Input
+        id="create-project-title"
+        name="title"
+        placeholder="e.g. Kitchen Makeover"
+        type="text"
+      />
+    </label>
+    <label htmlFor="create-project-location">
+      <br />
+      <br />Where are you building this project?<br />
+      <Input
+        id="create-project-location"
+        name="location"
+        placeholder="Start typing a location"
+        type="text"
+      />
+    </label>
+    <label htmlFor="create-project-label">
+      <br />
+      <br />What kind of project is it?<br />
+      <select disabled id="create-project-label" name="label">
+        <option>Remodel</option>
+        <option>Rebuild</option>
+        <option>Renovation</option>
+        <option>Outdoor Building</option>
+      </select>
+      <select id="create-project-type" name="type">
+        {projectTypes &&
+          projectTypes.map(projectType => (
+            <option key={projectType} value={projectType}>
+              {projectType}
+            </option>
+          ))}
+      </select>
+    </label>
+    <br />
+    <br />
+    <Button>Create</Button>
+  </form>
+)
+
+CreateForm.propTypes = {
+  projectTypes: PropTypes.array.isRequired,
+  onSubmit: PropTypes.func.isRequired
+}
+
+export default CreateForm
