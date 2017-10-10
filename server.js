fe1a62ffe570c07dcc3152fbb64b03022cd61c0b
Mike Nikles <mikenikles@gmail.com>
2017-10-09 18:06:07 -0700

    Add a tracking endpoint that connects to keen.io

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 /* eslint-env node */
 import next from 'next'
 import micro from 'micro'
+import microRoute from 'micro-route'
 import {nextRoutes} from './utils/routes'
+import {recordEvent} from './utils/tracking'
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const app = next({dev: process.env.NODE_ENV !== 'production'})
 const handler = nextRoutes.getRequestHandler(app)
 
+const trackRoute = microRoute('/_track/:collection', 'POST')
+
 const server = micro(async (req, res) => {
+  const trackRouteResult = trackRoute(req)
+  // FIXME: Only allow requests from www.dittly.co to prevent anyone from sending POST requests
+  if (trackRouteResult) {
+    const event = await micro.json(req)
+
+    // Send tracking data
+    recordEvent(trackRouteResult.params.collection, event)
+
+    // We're done here
+    return 'OK'
+  }
   return handler(req, res)
 })
 
